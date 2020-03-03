c81bc0af6580423617987b55710306907e762750
Ylgr <quyen.luminous@gmail.com>
2020-03-04 02:15:02 +0700

    (+) ichimoku res, sup, static, dynamic

--- ichimoku.js
+++ ichimoku.js
@@ -1,20 +1,126 @@
-function avgHighLow(marketInfos, timeget, candleTimeRange, sessions) {
-    const indexTimeget = marketInfos.find(marketInfo => marketInfo.time === timeget)
+const { range } = require('lodash');
+
+function avgHighLow(marketInfos, timeget, sessions) {
+    const indexTimeget = marketInfos.indexOf(marketInfos.find(marketInfo => marketInfo.time === timeget))
     if (indexTimeget - sessions < 0) return -1;
     const subInfos = marketInfos.slice(indexTimeget - sessions, indexTimeget);
-    const highestPrice = Math.max( marketInfos.map(marketInfo => marketInfo.high) );
-    const lowestPrice = Math.min( marketInfos.map(marketInfo => marketInfo.low) );
+    const highestPrice =  Math.max.apply(null, subInfos.map(marketInfo => marketInfo.high) );
+    const lowestPrice =  Math.min.apply(null, subInfos.map(marketInfo => marketInfo.low) );
     return (highestPrice + lowestPrice)/2;
 }
 
-module.exports = {
-    ichimokuCaculator : function (marketInfos, timeget, candleTimeRange) {
-        return {
-            tenkanSen: avgHighLow(marketInfos, 9),
-            kijunSen:  avgHighLow(marketInfos, 26),
-            senkouSpanA:  (avgHighLow(marketInfos, 9) + avgHighLow(marketInfos, 26))/2,
-            senkouSpanB: avgHighLow(marketInfos, 52)
+function ichimokuCaculator(marketInfos, timeget) {
+    const tenkanSen = avgHighLow(marketInfos, timeget, 9);
+    const kijunSen = avgHighLow(marketInfos, timeget, 26);
+    return {
+        tenkanSen: tenkanSen,
+        kijunSen:  kijunSen,
+        senkouSpanA:  (tenkanSen + kijunSen)/2,
+        senkouSpanB: avgHighLow(marketInfos, timeget, 52)
+    }
+}
+
+function ichimokuDynamicResistCaculator(marketInfos, timeget, timeRangeValue) {
+    const currentCacul = ichimokuCaculator(marketInfos, timeget)
+    const cacul26Before = ichimokuCaculator(marketInfos, timeget - timeRangeValue*24)
+    return {
+        tenkanSen: currentCacul.tenkanSen,
+        kijunSen: currentCacul.kijunSen,
+        senkouSpanA: cacul26Before.senkouSpanA,
+        senkouSpanB: cacul26Before.senkouSpanB
+    }
+}
+
+function ichimokuStaticResistCaculator(marketInfos, timeget, timeRangeValue, sessions = 100) {
+    const tenkanSenResists = [];
+    const kijunSenResists = [];
+    const senkouSpanBResists = [];
+
+    let checkTenkanSenResist = {
+        price: 0,
+        loopTime: 0
+    }
+    let checkKijunSenResist = {
+        price: 0,
+        loopTime: 0
+    }
+    let checkSenkouSpanBResist = {
+        price: 0,
+        loopTime: 0
+    }
+    let i;
+    for(i = 0; i < sessions; i ++) {
+        const checking = ichimokuCaculator(marketInfos, timeget - timeRangeValue*i)
+        if (checkTenkanSenResist.price === checking.tenkanSen) {
+            if (checkTenkanSenResist.loopTime >= 3 && tenkanSenResists.indexOf(checkTenkanSenResist.price) === -1) {
+                tenkanSenResists.push(checkTenkanSenResist.price)
+            }
+            checkTenkanSenResist.loopTime++
+        } else {
+            checkTenkanSenResist.price = checking.tenkanSen
+        }
+        if (checkKijunSenResist.price === checking.kijunSen) {
+            if (checkKijunSenResist.loopTime >= 3 && kijunSenResists.indexOf(checkKijunSenResist.price) === -1) {
+                kijunSenResists.push(checkKijunSenResist.price)
+            }
+            checkKijunSenResist.loopTime++
+        } else {
+            checkKijunSenResist.price = checking.kijunSen
+        }
+        if (checkSenkouSpanBResist.price === checking.senkouSpanB) {
+            if (checkSenkouSpanBResist.loopTime >= 3 && senkouSpanBResists.indexOf(checkSenkouSpanBResist.price) === -1) {
+                senkouSpanBResists.push(checkSenkouSpanBResist.price)
+            }
+            checkSenkouSpanBResist.loopTime++
+        } else {
+            checkSenkouSpanBResist.price = checking.senkouSpanB
         }
     }
+    return {
+        tenkanSenResists: tenkanSenResists,
+        kijunSenResists: kijunSenResists,
+        senkouSpanBResists: senkouSpanBResists
+    }
+}
+
+function getClosestResists(currentPrice, marketInfos, timeget, timeRangeValue, sessions = 100) {
+    const resists = ichimokuStaticResistCaculator(marketInfos, timeget, timeRangeValue, sessions);
+
+    const closestResistTenkanSen = resists.tenkanSenResists.reduce(function(prev, curr) {
+        return (Math.abs(curr - currentPrice) > Math.abs(prev - currentPrice) ? curr : prev);
+    });
+    const closestSupportTenkanSen = resists.tenkanSenResists.reduce(function(prev, curr) {
+        return (Math.abs(curr - currentPrice) < Math.abs(prev - currentPrice) ? curr : prev);
+    });
+    
+    const closestResistKijunSen = resists.kijunSenResists.reduce(function(prev, curr) {
+        return (Math.abs(curr - currentPrice) > Math.abs(prev - currentPrice) ? curr : prev);
+    });
+    const closestSupportKijunSen = resists.kijunSenResists.reduce(function(prev, curr) {
+        return (Math.abs(curr - currentPrice) < Math.abs(prev - currentPrice) ? curr : prev);
+    });
+    
+    const closestResistSenkouSpanB = resists.senkouSpanBResists.reduce(function(prev, curr) {
+        return (Math.abs(curr - currentPrice) > Math.abs(prev - currentPrice) ? curr : prev);
+    });
+    const closestSupportSenkouSpanB = resists.senkouSpanBResists.reduce(function(prev, curr) {
+        return (Math.abs(curr - currentPrice) < Math.abs(prev - currentPrice) ? curr : prev);
+    });
+    return {
+        closestResistTenkanSen: closestResistTenkanSen,
+        closestSupportTenkanSen: closestSupportTenkanSen,
+        closestResistKijunSen: closestResistKijunSen,
+        closestSupportKijunSen: closestSupportKijunSen,
+        closestResistSenkouSpanB: closestResistSenkouSpanB,
+        closestSupportSenkouSpanB: closestSupportSenkouSpanB,
+    }
+}
+
+
+module.exports = {
+    ichimokuCaculator,
+    ichimokuDynamicResistCaculator,
+    ichimokuStaticResistCaculator,
+    getClosestResists
 }
 
--- index.js
+++ index.js
@@ -41,15 +41,15 @@ const telegramToken = '933051371:AAF4sE3732Rq4KaCJCPHn5zynOedYJPUmQo';
 const testChannelId = '-1001348986056';
 
 function telegramMessageRequest(channelId, text) {
-  return instance.get('https://api.telegram.org/bot' + telegramToken + '/sendMessage?chat_id=' + channelId + '&text=' + text)
+  return instance.get('https://api.telegram.org/bot' + telegramToken + '/sendMessage?chat_id=' + channelId + '&text=' + encodeURIComponent(text))
 }
 
 const now = Date.now();
 candleTimeRangeMap.forEach((timeRangeValue, timeRangeKey) => {
   const roundTimeNow = now - now % timeRangeValue;
-  const timeIn52SessionBefore = roundTimeNow - timeRangeValue * 52;
+  const timeIn500SessionBefore = roundTimeNow - timeRangeValue * 500;
   symbolList.forEach(symbol => {
-    const dataRequest = usingRealTimeApiData ? instance.get(candleStickBuildParam(symbol, timeRangeKey, timeIn52SessionBefore, roundTimeNow))
+    const dataRequest = usingRealTimeApiData ? instance.get(candleStickBuildParam(symbol, timeRangeKey, timeIn500SessionBefore, roundTimeNow))
       : readFile('./' + mockFileName)
 
     dataRequest.then(response => {
@@ -67,12 +67,31 @@ candleTimeRangeMap.forEach((timeRangeValue, timeRangeKey) => {
       })
 
       const tdResult = TDSequential(marketInfos);
-      const ichimokuResponse = ichimoku.ichimokuCaculator(marketInfos, roundTimeNow, timeRangeValue);
-
-      console.log("TDSequential result: ", JSON.stringify(tdResult, null, 2));
-      console.log("Ichimoku result: ",ichimokuResponse);
-      console.log(tdResult[tdResult.length - 1].sellSetupIndex)
-      telegramMessageRequest(testChannelId, 'TDSequential notification: ' + tdResult[tdResult.length - 1].sellSetupIndex).catch(console.log)
+      const ichimokuResponse = ichimoku.ichimokuCaculator(marketInfos, roundTimeNow);
+      const ichimokuDynamicResist = ichimoku.ichimokuDynamicResistCaculator(marketInfos, roundTimeNow, timeRangeValue);
+      const ichimokuStaticResist = ichimoku.ichimokuStaticResistCaculator(marketInfos, roundTimeNow, timeRangeValue);
+      const ichimokuStaticClosestPriceResist =ichimoku.getClosestResists(8688 , marketInfos, roundTimeNow, timeRangeValue)
+      const lastTdResult = tdResult[tdResult.length - 1];
+      // console.log("TDSequential result: ", JSON.stringify(tdResult[tdResult.length - 1], null, 2));
+      // console.log("Ichimoku result: ", ichimokuResponse);
+      // console.log("Ichimoku Dynamic Res result: ", ichimokuDynamicResist);
+      // console.log("Ichimoku Static Res result: ", ichimokuStaticResist);
+      // console.log("Current Res result: ", ichimokuStaticClosestPriceResist);
+      telegramMessageRequest(testChannelId, 'Khung 1h Binance: ');
+      telegramMessageRequest(testChannelId, 'Tín hiệu TD Sequential: \n' + 
+      `Chỉ số mua: ${lastTdResult.buySetupIndex} \n` +
+      `Chỉ số bán: ${lastTdResult.sellSetupIndex} \n`
+      );
+      telegramMessageRequest(testChannelId, 'Tín hiệu Ichimoku: \n' + 
+      `Kháng cự động: ${JSON.stringify(ichimokuDynamicResist, null, 2)} \n` +
+      `Kháng cự tĩnh gần giá nhất: \n` + 
+      `Tenkan Sen - Kháng cự: ${ichimokuStaticClosestPriceResist.closestResistTenkanSen}\n` +
+      `Tenkan Sen - Hỗ trợ: ${ichimokuStaticClosestPriceResist.closestSupportTenkanSen}\n` +
+      `Kijun Sen - Kháng cự: ${ichimokuStaticClosestPriceResist.closestResistKijunSen}\n` +
+      `Kijun Sen - Hỗ trợ: ${ichimokuStaticClosestPriceResist.closestSupportKijunSen}\n` +
+      `Senkou Span B - Kháng cự: ${ichimokuStaticClosestPriceResist.closestResistSenkouSpanB}\n` +
+      `Senkou Span B - Hỗ trợ: ${ichimokuStaticClosestPriceResist.closestSupportSenkouSpanB}\n`
+      );
     }).catch(err => console.log('Error: ', err));
   })
 });
